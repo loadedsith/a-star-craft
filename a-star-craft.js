40599491fb9014027df68ecfeea73388f646ae39
Graham Heath <heathg@google.com>
2018-10-19 21:44:41 -0500

    2

--- a-star-craft.js
+++ a-star-craft.js
@@ -2,26 +2,91 @@
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
+/*
+  Collect islands
+  Set up islands with
+  ************
+  *>        <*
+  *>        <*
+  *>        <*
+  *>        <*
+  *>        <*
+  *>        <*
+  *>        <*
+  ************
+
+  ************
+  *>         *
+  * \/       *
+  *  >       *
+  *   \/     *
+  *    >     *
+  *     \/   *
+  *      >   *
+  ************
+*/
 const lines = [];
 let x0 = 0;
 let y0 = 0;
 let length = 0;
 
+const actions = [
+  {
+    action: (x, y, cell, lines) => {
+      return "R"
+    },
+    matcher: (x, y, cell, lines) => {
+      if (cell == '.') {
+        debugger;
+
+        if (lines[x - 1][y] == '#' ) {
+          if (lines[x + 1][y] == '#' ) {
+            if (lines[x][y - 1] == '#' ) {
+              printErr('gotone');
+              return true;
+            }
+          }
+        }
+      }
+      return false;
+    },
+    name: `
+     #
+    #P
+     #
+    `,
+  }
+];
+
 for (let i = 0; i < 10; i++) {
   const line = readline();
-  lines.push(line);
+  lines.push([...line]);
+  // printErr('lines', lines.join('\n'));
   let firstDot = line.indexOf('.');
   if (!x0 && firstDot > -1) {
     x0 = firstDot;
     y0 = i;
     length = line.match(/\./g).length;
   }
-  printErr('line', line)
-  printErr('length', length)
+  printErr('length', length);
 }
 
 printErr('x0', x0, 'y0', y0);
 
+const commands = [];
+
+lines.forEach((line, x) => {
+  printErr('line', line);
+  line.forEach((cell, y) => {
+    actions.forEach((action) => {
+      if (action.matcher(x, y, cell, lines)) {
+        commands.push(x, y, action.action(x, y, cell, lines));
+      }
+    })
+  })
+})
+printErr('commands',commands.join(' '))
 const startPoint = [x0, y0];
 const robotCount = parseInt(readline());
 
