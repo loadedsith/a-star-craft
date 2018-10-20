a7eb392b696e3fe50476777539e2d5eec9ea5038
Graham Heath <heathg@google.com>
2018-10-20 12:23:19 -0500

    6

--- a-star-craft.js
+++ a-star-craft.js
@@ -1,3 +1,5 @@
+/* global printErr readline */
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -19,28 +21,62 @@
 */
 const lines = [];
 const commands = [];
-
-const addCommand = (x, y, command) => {
-  if (!commands[y]) {
-    commands[y] = []
-  }
-  commands[y][x] = command;
-}
+const mod = (a, n) => {
+  return a - n * Math.floor(a/n);
+};
 
 const getNeighbors = (x, y, cell, lines) => {
+  const northLine = lines[mod(y - 1, lines.length)];
+  const southLine = lines[mod(y + 1, lines.length)];
+  const eastI = x + 1;
+  const westI = x - 1;
+
   return {
     // x is e-w
     // y is n-s
-    e:  (lines[y]     || [])[x + 1] || false,
-    n:  (lines[y - 1] || [])[x]     || false,
-    ne: (lines[y - 1] || [])[x + 1] || false,
-    nw: (lines[y - 1] || [])[x - 1] || false,
-    s:  (lines[y + 1] || [])[x]     || false,
-    se: (lines[y + 1] || [])[x + 1] || false,
-    sw: (lines[y + 1] || [])[x - 1] || false,
-    w:  (lines[y]     || [])[x - 1] || false,
+    e:  (lines[y]  || [])[mod(eastI, lines[y].length)] || false,
+    n:  (northLine || [])[x]     || false,
+    ne: (northLine || [])[mod(eastI, northLine.length)] || false,
+    nw: (northLine || [])[mod(westI, northLine.length)] || false,
+    s:  (southLine || [])[x]     || false,
+    se: (southLine || [])[mod(eastI, southLine.length)] || false,
+    sw: (southLine || [])[mod(westI, southLine.length)] || false,
+    w:  (lines[y]  || [])[mod(westI, lines[y].length)] || false,
+  };
+};
+
+const isCommandSafe = function(x, y, cell, command) {
+  let safe = false;
+  const {n, w, s, e} = getNeighbors(x, y, cell, lines);
+
+  switch (command) {
+    case 'R':
+      safe = e != '#';
+      break;
+    case 'L':
+      safe = w != '#';
+      break;
+    case 'U':
+      safe = n != '#';
+      break;
+    case 'D':
+      safe = s != '#';
+      break;
   }
-}
+
+  return safe;
+};
+
+const addCommand = (x, y, cell, command) => {
+  if (!commands[y]) {
+    commands[y] = [];
+  }
+
+  if (isCommandSafe(x, y, cell, command)) {
+    commands[y][x] = command;
+  }
+};
+
 
 let x0 = 0;
 let y0 = 0;
@@ -49,20 +85,14 @@ let length = 0;
 const actions = [
   {
     action: (x, y, cell, lines) => {
-      addCommand(x, y, 'R');
+      addCommand(x, y, cell, 'R');
     },
     matcher: (x, y, cell, lines) => {
       if (cell == '.') {
         const {n, w, s} = getNeighbors(x, y, cell, lines);
-        printErr('n, w, s', n, w, s)
-        if (n == '#') {
-          if (w == '#') {
-            if (s == '#') {
-              printErr('got R');
 
-              return true;
-            }
-          }
+        if (n == '#' && w == '#' && s == '#') {
+          return true;
         }
       }
 
@@ -76,20 +106,14 @@ const actions = [
   },
   {
     action: (x, y, cell, lines) => {
-      addCommand(x, y, 'L');
+      addCommand(x, y, cell, 'L');
     },
     matcher: (x, y, cell, lines) => {
       if (cell == '.') {
         const {n, e, s} = getNeighbors(x, y, cell, lines);
-        printErr('n, e, s', n, e, s)
-        if (n == '#' ) {
-          if (s == '#') {
-            if (e == '#') {
-              printErr('got L');
 
-              return true;
-            }
-          }
+        if (n == '#' && s == '#' && e == '#') {
+          return true;
         }
       }
 
@@ -103,7 +127,7 @@ const actions = [
   },
   {
     action: (x, y, cell, lines) => {
-      addCommand(x, y, 'R');
+      addCommand(x, y, cell, 'R');
     },
     matcher: (x, y, cell, lines) => {
       if (cell == '.') {
@@ -126,12 +150,11 @@ const actions = [
   },
   {
     action: (x, y, cell, lines) => {
-      addCommand(x, y, 'D');
+      addCommand(x, y, cell, 'D');
     },
     matcher: (x, y, cell, lines) => {
       if (cell == '.') {
         const {n, e, sw, w, s} = getNeighbors(x, y, cell, lines);
-
         if (n == '#' && e == '#' && sw == '#') {
           if (w == '.' && s == '.') {
             return true;
@@ -149,7 +172,7 @@ const actions = [
   },
   {
     action: (x, y, cell, lines) => {
-      addCommand(x, y, 'L');
+      addCommand(x, y, cell, 'L');
     },
     matcher: (x, y, cell, lines) => {
       if (cell == '.') {
@@ -172,7 +195,7 @@ const actions = [
   },
   {
     action: (x, y, cell, lines) => {
-      addCommand(x, y, 'U');
+      addCommand(x, y, cell, 'U');
     },
     matcher: (x, y, cell, lines) => {
       if (cell == '.') {
@@ -196,8 +219,8 @@ const actions = [
 
   {
     action: (x, y, cell, lines) => {
-      addCommand(x, y, 'D');
-      addCommand(x, y, 'D');
+      addCommand(x, y, cell, 'D');
+      // addCommand(x, y + 1, cell, 'D');
     },
     matcher: (x, y, cell, lines) => {
       if (cell == '.') {
@@ -220,7 +243,7 @@ const actions = [
   },
   {
     action: (x, y, cell, lines) => {
-      addCommand(x, y, 'U');
+      addCommand(x, y, cell, 'U');
     },
     matcher: (x, y, cell, lines) => {
       if (cell == '.') {
@@ -238,6 +261,77 @@ const actions = [
     name: `
     PPP
      P
+     #
+    `,
+  },
+  {
+    action: (x, y, cell, lines) => {
+      addCommand(x, y, cell, 'R');
+    },
+    matcher: (x, y, cell, lines) => {
+      if (cell == '.') {
+        const {ne, w, s, n, e} = getNeighbors(x, y, cell, lines);
+
+        if (w == '#' && s == '#') {
+          if (n == 'D') {
+            return true;
+          }
+        }
+      }
+
+      return false;
+    },
+    name: `
+     D
+    #P
+     #
+    `,
+  },
+
+  {
+    action: (x, y, cell, lines) => {
+      addCommand(x, y, cell, 'L');
+    },
+    matcher: (x, y, cell, lines) => {
+      if (cell == '.') {
+        const {ne, w, s, n, e} = getNeighbors(x, y, cell, lines);
+
+        if (e == '#' && s == '#') {
+          if (n == 'D') {
+            return true;
+          }
+        }
+      }
+
+      return false;
+    },
+    name: `
+     D
+     P#
+     #
+    `,
+  },
+
+  {
+    action: (x, y, cell, lines) => {
+      addCommand(x, y, cell, 'L');
+    },
+    matcher: (x, y, cell, lines) => {
+      if (cell == '.') {
+        const {ne, w, s, n, e} = getNeighbors(x, y, cell, lines);
+
+        if (e == '#' && s == '#') {
+          if (n == 'D') {
+            return true;
+          }
+        }
+      }
+
+      return false;
+    },
+    name: `
+     D
+     P#
      #
     `,
   },
@@ -261,8 +355,9 @@ for (let i = 0; i < 10; i++) {
 printErr('x0', x0, 'y0', y0);
 
 lines.forEach((line, y) => {
-  printErr('line', line);
-
+  printErr('line', line.join(''));
+});
+lines.forEach((line, y) => {
   line.forEach((cell, x) => {
     actions.forEach((action) => {
       if (action.matcher(x, y, cell, lines)) {
@@ -275,10 +370,10 @@ lines.forEach((line, y) => {
 const getCommandString = (commands) => {
   return commands.reduce((acc, item, y) => {
     return acc + item.reduce((bcc, item, x) => {
-      return bcc + `${x} ${y} ${item} `
-    }, '')
+      return bcc + `${x} ${y} ${item} `;
+    }, '');
   }, '');
-}
+};
 
 printErr('commands', getCommandString(commands));
 
@@ -287,20 +382,63 @@ const robotCount = parseInt(readline());
 
 let robots = [];
 
-let Robot = function(config={}) {
-  return Object.assign({}, config);
-};
+/** Soft kitty warm kitty. */
+class Robot {
+  /**
+   * Little ball of fur.
+   * @param {Object=} config Happy kitty.
+   */
+  constructor(config = {}) {
+    return Object.assign(this, config);
+  }
+}
 
 for (let i = 0; i < robotCount; i++) {
-  var inputs = readline().split(' ');
+  const inputs = readline().split(' ');
   const x = parseInt(inputs[0]);
   const y = parseInt(inputs[1]);
   const direction = inputs[2];
-  robots.push([Robot({x, y, direction})]);
+
+  robots.push(new Robot({
+    direction,
+    x,
+    y,
+  }));
 }
 
 printErr('robots', JSON.stringify(robots));
+robots.forEach(({x, y, direction}) => {
+  printErr('x, y, direction', x, y, direction);
+
+  if (!isCommandSafe(x, y, '.', direction)) {
+    let newDir;
+    switch (direction) {
+      case 'U':
+        newDir = 'D';
+        break;
+      case 'D':
+        newDir = 'U';
+        break;
+      case 'R':
+        newDir = 'L';
+        break;
+      case 'L':
+        newDir = 'R';
+        break;
+    }
+    addCommand(x, y, '.', newDir);
+  }
+});
+
+robots.forEach(({x, y, direction}) => {
+  const neighbors = getNeighbors(x, y, '.', lines);
+  const scores = {};
+
+  Object.keys(neighbors).forEach((key) => {
+    const dir = neighbors[key];
+  });
+});
 
 // Write an action using print()
 // To debug: printErr('Debug messages...');
-print(getCommandString(commands) + '17 7 U');
+print(getCommandString(commands));
